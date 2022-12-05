9ac31cd2a441cfdab04019b0534ff4f35bd04ad3
Yasmin Santos <yasminnas989@gmail.com>
2022-12-05 11:24:47 -0300

    05/12/2022

---
+++ Front-End/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile.menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../img/menu_icon_png";
+    }else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../img/close_white_36dp.svg";
+    }
+}
